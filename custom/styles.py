d5789d9c89667a22e2e8541d859324bc17154678
Jessica Nash <janash@vt.edu>
2024-07-14 12:42:15 -0400

    add custom folder

---
+++ custom/styles.py
@@ -0,0 +1,8 @@
+"""Load custom styles"""
+
+style_string = """
+ <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" /><meta name="generator" content="Docutils 0.19: https://docutils.sourceforge.io/" />
+
+    <link rel="stylesheet" type="text/css" href="custom/custom.css" />
+"""
